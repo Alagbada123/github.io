3251af80cbc21c37e2a742bf8dbc7b4e4eaeb254
Alagbada123 <114651605+Alagbada123@users.noreply.github.com>
2024-08-01 18:39:48 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     $(window).scroll(function() {
         // sticky navbar on scroll script
-        if (this.scrollY > 20) {
+        if ($(this).scrollTop() > 20) {
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass("sticky");
         }
 
         // scroll-up button show/hide script
-        if (this.scrollY > 500) {
+        if ($(this).scrollTop() > 500) {
             $('.scroll-up-btn').addClass("show");
         } else {
             $('.scroll-up-btn').removeClass("show");
@@ -17,13 +17,13 @@ $(document).ready(function() {
 
     // slide-up script
     $('.scroll-up-btn').click(function() {
-        $('html').animate({ scrollTop: 0 });
+        $('html, body').animate({ scrollTop: 0 }, 'smooth');
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function() {
-        // applying again smooth scroll on menu items click
+        // applying smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
